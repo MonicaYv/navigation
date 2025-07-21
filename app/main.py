dc6cfb94f4787894091e4393625f8c01e92754dc
Mona <monig5658@gmail.comm>
2025-07-21 17:04:41 +0530

    company route removed

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 
 from .auth import router as auth_router
-from .company_auth import router as company_auth_router
+#from .company_auth import router as company_auth_router
 from .user_maps import router as user_maps_router
 
 app = FastAPI()
 
 app.include_router(auth_router)
-app.include_router(company_auth_router)
+#app.include_router(company_auth_router)
 app.include_router(user_maps_router)
 
 @app.get("/")
